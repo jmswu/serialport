187243bbeaed8cbb11ec51c59a13673efa897aad
James Wu <jameswu62@yahoo.co.nz>
2021-03-03 09:57:01 +1300

    add TEST_SERIALPORT_SEND_AND_RECEIVE_ONE_BYTE()

--- src/main.c
+++ src/main.c
@@ -15,6 +15,8 @@ void TEST_SERIALPORT_CONNECT_SUCCESSFULLY(void);
 void TEST_SERIALPORT_CONNECT_WRONG_PARITY(void);
 void TEST_SERIALPORT_CONNECT_WRONG_STOPBITS(void);
 void TEST_SERIALPORT_CONNECT_SIMPLE_SUCCESSFULLY(void);
+void TEST_SERIALPORT_CONNECT_SIMPLE_NONE_EXIST_PORT_NUMBER(void);
+void TEST_SERIALPORT_SEND_AND_RECEIVE_ONE_BYTE(void);
 
 int main(int argc, char **argv)
 {
@@ -27,6 +29,8 @@ int main(int argc, char **argv)
     TEST_SERIALPORT_CONNECT_WRONG_PARITY();
     TEST_SERIALPORT_CONNECT_WRONG_STOPBITS();
     TEST_SERIALPORT_CONNECT_SIMPLE_SUCCESSFULLY();
+    TEST_SERIALPORT_CONNECT_SIMPLE_NONE_EXIST_PORT_NUMBER();
+    TEST_SERIALPORT_SEND_AND_RECEIVE_ONE_BYTE();
 
     return test_done_message();
 }
@@ -91,5 +95,30 @@ void TEST_SERIALPORT_CONNECT_SIMPLE_SUCCESSFULLY(void)
 {
     HANDLE handle = serialPort_connectSimple(PORT_NUM_LOOP_BACK, 9600);
     TEST_ASSERT_NOT_EQUAL(INVALID_HANDLE_VALUE, handle);
+    serialPort_disconnect(handle);
+}
+
+void TEST_SERIALPORT_CONNECT_SIMPLE_NONE_EXIST_PORT_NUMBER(void)
+{
+    HANDLE handle_1 = serialPort_connectSimple(PORT_NUM_NONE_EXIST_1, 9600);
+    TEST_ASSERT_EQUAL(INVALID_HANDLE_VALUE, handle_1);
+    serialPort_disconnect(handle_1);
+
+    HANDLE handle_2 = serialPort_connectSimple(PORT_NUM_NONE_EXIST_2, 9600);
+    TEST_ASSERT_EQUAL(INVALID_HANDLE_VALUE, handle_2);
+    serialPort_disconnect(handle_2);
+}
+
+void TEST_SERIALPORT_SEND_AND_RECEIVE_ONE_BYTE(void)
+{
+    HANDLE handle = serialPort_connectSimple(PORT_NUM_LOOP_BACK, 19200);
+
+    for(unsigned i = 0; i < 256; i++)
+    {
+        serialPort_sendByte(handle, i);
+        uint8_t rxData = serialPort_getOneByte(handle);
+        TEST_ASSERT_EQUAL_UINT8(i, rxData);
+    }
+
     serialPort_disconnect(handle);
 }
