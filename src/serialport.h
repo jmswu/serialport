a32cf1834a4011eb7aa34875c3a2bccdea47d6c0
James Wu <jameswu62@yahoo.co.nz>
2021-03-02 15:46:19 +1300

    added some API

--- src/serialport.h
+++ src/serialport.h
@@ -6,10 +6,20 @@ extern "C" {
 #endif
 
 #include <stdint.h>
+#include <Windows.h>
 
-uint32_t serialPort_getLibraryVersion(void);
+#define INVALID_HANDLE_VALUE    ((HANLE) -1)
 
-int serialPort_connect(unsigned portNumber, unsigned baud);
+uint32_t    serialPort_getLibraryVersion(void);
+
+HANDLE      serialPort_connect(unsigned portNumber, unsigned baud, unsigned dataBits, unsigned priority, unsigned stopBits);
+HANDLE      serialPort_connectSimple(unsigned portNumber, unsigned baud);
+void        serialPort_disconnect(HANDLE handle);
+void        serialPort_sendByte(HANDLE handle, uint8_t data);
+void        serialPort_sendArray(HANDLE handle, uint8_t *data, size_t dataLength);
+int         serialPort_getNumberOfBytes(HANDLE handle);
+uint8_t     serialPort_getOneByte(HANDLE handle);
+int         serialPort_getArray(HANDLE handle, uint8_t *data, size_t dataLength);
 
 #ifdef __cplusplus
 }
